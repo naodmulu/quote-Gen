84cc3bd843086a9c51b0ead2b48d21a40f1af20e
naodmulu <naodmulugeta1621@gmail.com>
2024-10-25 15:39:25 +0300

    Add: Functions to Bottons

---
+++ config.js
@@ -0,0 +1,2 @@
+// config.js
+const API_KEY = "NcGLpoltMvsKEXgZlUH71w==f7t1D8UUP5i4kp0j";
--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-const getQuote() => {
+const quoteContainer = document.getElementById("quoteContainer")
+const quoteText = document.getElementById("quote")
+const quoteAuthor = document.getElementById("author")
+const tweetBtn = document.getElementById("twitter")
+const nextBtn = document.getElementById("next")
+let quotes = [];
+const newQuote = async () => {
+    await getQuotes()
+    const quote =  quotes[0]
+    quoteText.textContent =  quote.quote 
+    quoteAuthor.textContent =  quote.author 
+    
+}
 
-    const URL = "https://api.api-ninjas.com/v1/quotes?category=learning"
-
-    try{
-        const response = await fetch(URL);
-        data = await response.json()
-        console.log("hello")
-    }
-    catch (error){
-
+async function getQuotes() {
+    const URL = "https://api.api-ninjas.com/v1/quotes?category=learning";
+    try {
+        const response = await fetch(URL, {
+            method: "GET",
+            headers: {
+                'X-Api-Key': API_KEY,
+                'Content-Type': 'application/json'
+            }
+        });
+        quotes = await response.json();
+        console.log("hello", typeof(quotes[0].quote));
+    } catch (error) {
+        console.error("Error fetching quotes:", error);
     }
 }
+
+const tweetQuote = () => {
+    const twitterURL = `https://twitter.com/intent/tweet?text=${quoteText.textContent}%0A-By ${quoteAuthor.textContent}`;
+    window.open(twitterURL, '_blank');
+};
+
+
+tweetBtn.addEventListener('click',tweetQuote)
+nextBtn.addEventListener('click',newQuote);
+// newQuote();
