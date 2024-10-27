147ab3be34529c86bc22850ee2306ce4ed723523
naodmulu <foordownload@gmail.com>
2024-10-27 15:45:33 +0300

    Fix: uncommented script and removed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,9 @@ module.exports = {
     extend: {
       screens: {
         sm: '640px',
-        md: '768px', // medium screen size
-        lg: '1024px', // large screen size
-        xl: '1280px', // extra-large screen size
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
       },
     },
     plugins: [],
